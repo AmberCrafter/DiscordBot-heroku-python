81cf84aea4b6e048a300df12275c663108c7d5e8
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-07-14 06:03:28 +0000

    update descriptions

--- extensions/classer.py
+++ extensions/classer.py
@@ -2,10 +2,16 @@
 Basic object which had import necessary libs and setup status.
 This is useful on develop cog.
 
+cog style:
+    - bot.command() -> commands.command()
+    - bot.event -> commands.Cog.listener()
+
 Usage:
 from extensions.classer import Ext_Cog
 import discord
 from discord.ext import commands
+class CLASSNAME(Ext_Cog):
+    ...
 """
 import discord
 from discord.ext import commands
