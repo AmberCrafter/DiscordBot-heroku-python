b579c2bf70564caec99b6b2a3b5b7521628e95da
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-07-26 16:36:16 +0000

    finished quest ui

--- main.py
+++ main.py
@@ -1,52 +1,129 @@
-from builtins import print
+# Rewrite the main script into async version
+# ref. https://github.com/Rapptz/discord.py/blob/master/examples/advanced_startup.py
+
 from dotenv import load_dotenv
+import asyncio
+import logging
+import logging.handlers
 import os
-import datetime
+from typing import List, Optional
+
+import discord
+from discord.ext import commands
+from aiohttp import ClientSession
 
 # get bot token
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-import discord
-from discord.ext import commands
+COMMAND_PERFIX = '?'
+DISCRIPTION = '''Development assistant!'''
+INTENTS = discord.Intents.default()
+INTENTS.members = True
 
-description = '''Development assistant!'''
+class CustomBot(commands.Bot):
+    def __init__(
+        self,
+        *args,
+        initial_extensions: List[str],
+        # db_pool: asyncpg.Pool,
+        # web_client: ClientSession,
+        testing_guild_id: Optional[int] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        # self.db_pool = db_pool
+        # self.web_client = web_client
+        self.testing_guild_id = testing_guild_id
+        self.initial_extensions = initial_extensions
 
-intents = discord.Intents.default()
-intents.members = True
+    async def setup_hook(self) -> None:
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+        # here, we are loading extensions prior to sync to ensure we are syncing interactions defined in those extensions.
 
-@bot.command()
-async def load(ctx, extension):
-    """load extensions"""
-    bot.load_extension(f"extensions.{extension}")
-    await ctx.send(f"Loaded {extension} successful.")
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
 
-@bot.command()
-async def unload(ctx, extension):
-    """unload extensions"""
-    bot.unload_extension(f"extensions.{extension}")
-    await ctx.send(f"Unloaded {extension} successful.")
+        # In overriding setup hook,
+        # we can do things that require a bot prior to starting to process events from the websocket.
+        # In this case, we are using this to ensure that once we are connected, we sync for the testing guild.
+        # You should not do this for every guild or for global sync, those should only be synced when changes happen.
+        if self.testing_guild_id:
+            guild = discord.Object(self.testing_guild_id)
+            # We'll copy in the global commands to test with:
+            self.tree.copy_global_to(guild=guild)
+            # followed by syncing to the testing guild.
+            await self.tree.sync(guild=guild)
 
-@bot.command()
-async def reload(ctx, extension):
-    """reload extensions"""
-    bot.reload_extension(f"extensions.{extension}")
-    await ctx.send(f"Reloaded {extension} successful.")
+        # This would also be a good place to connect to our database and
+        # load anything that should be in memory prior to handling events.
 
-# init module
-folderpath = './extensions'
-for filename in os.listdir(folderpath):
-    if not filename.endswith('.py'): continue
-    if filename in ['classer.py']: continue
-    # load_extansion path format: {folder}.{file}
-    #   which 
-    #   root path is same as main.py location
-    #   folder and file seperate with "."
-    #   file didn't need suffix
-    bot.load_extension(f"{folderpath[2:]}.{filename[:-3]}")
+async def main():
+    # When taking over how the bot process is run, you become responsible for a few additional things.
+    # 1. logging
+    # for this example, we're going to set up a rotating file logger.
+    # for more info on setting up logging,
+    # see https://discordpy.readthedocs.io/en/latest/logging.html and https://docs.python.org/3/howto/logging.html
 
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename='./log/discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # One of the reasons to take over more of the process though
+    # is to ensure use with other libraries or tools which also require their own cleanup.
+
+    # Here we have a web client and a database pool, both of which do cleanup at exit.
+    # We also have our bot, which depends on both of these.
+
+    # async with ClientSession() as our_client, asyncpg.create_pool(user='postgres', command_timeout=30) as pool:
+    #     # 2. We become responsible for starting the bot.
+    #     exts = ['general', 'mod', 'dice']
+    #     async with CustomBot(commands.when_mentioned, db_pool=pool, web_client=our_client, initial_extensions=exts) as bot:
+    #         await bot.start(os.getenv('TOKEN', ''))
+    exts = []
+    # init module
+    folderpath = './extensions'
+    for filename in os.listdir(folderpath):
+        if not filename.endswith('.py'): continue
+        if filename in ['classer.py']: continue
+        # load_extansion path format: {folder}.{file}
+        #   which 
+        #   root path is same as main.py location
+        #   folder and file seperate with "."
+        #   file didn't need suffix
+        exts.append(f"{folderpath[2:]}.{filename[:-3]}")
+
+    async with CustomBot(commands.when_mentioned, description=DISCRIPTION, intents=INTENTS, initial_extensions=exts) as bot:
+        @bot.command()
+        async def load(ctx, extension):
+            """load extensions"""
+            await bot.load_extension(f"extensions.{extension}")
+            await ctx.send(f"Loaded {extension} successful.")
+
+        @bot.command()
+        async def unload(ctx, extension):
+            """unload extensions"""
+            await bot.unload_extension(f"extensions.{extension}")
+            await ctx.send(f"Unloaded {extension} successful.")
+
+        @bot.command()
+        async def reload(ctx, extension):
+            """reload extensions"""
+            await bot.reload_extension(f"extensions.{extension}")
+            await ctx.send(f"Reloaded {extension} successful.")
+
+        await bot.start(TOKEN)
 
 if __name__=="__main__":
-    bot.run(TOKEN)
+    # For most use cases, after defining what needs to run, we can just tell asyncio to run it:
+    asyncio.run(main())
